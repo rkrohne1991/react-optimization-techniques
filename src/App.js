a267ff0c9652aa09d90587033453734e555d099f
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-09-28 07:58:38 +0100

    A Closer Look At Child Component Re-Evaluation & Preventing Unnecessary Re-Evaluations with React.memo()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App RUNNING");
 
   const toggleParagraphHandler = () => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
@@ -13,7 +17,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
