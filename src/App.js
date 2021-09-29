80173fbcdd2cdc85d30778882cca6011f44b6121
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-09-29 07:43:46 +0100

    Optimizing with useMemo()

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import Button from "./components/UI/Button/Button";
-import DemoOutput from "./components/Demo/DemoOutput";
+// import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 
 import "./App.css";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  // const [showParagraph, setShowParagraph] = useState(false);
+  // const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
-  console.log("App RUNNING");
+  // console.log("App RUNNING");
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
+  // const toggleParagraphHandler = useCallback(() => {
+  //   if (allowToggle) {
+  //     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  //   }
+  // }, [allowToggle]);
 
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  }
+  // const allowToggleHandler = () => {
+  //   setAllowToggle(true);
+  // };
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      {/* <h1>Hi there!</h1> */}
+      {/* <DemoOutput show={showParagraph} /> */}
+      {/* <Button onClick={allowToggleHandler}>Allow Toggle</Button> */}
+      {/* <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button> */}
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
